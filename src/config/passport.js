5c243c41dccfab77aafa97a260a448570522936a
PanchoCornejo <jofracocapancho@gmail.com>
2022-09-07 03:13:41 -0300

    Adaptacion SICAAL

--- src/config/passport.js
+++ src/config/passport.js
@@ -13,13 +13,13 @@ passport.use(
       const user = await User.findOne({ email: email });
 
       if (!user) {
-        return done(null, false, { message: "Not User found." });
+        return done(null, false, { message: "Usuario no encontrado." });
       }
 
       // Match Password's User
       const isMatch = await user.matchPassword(password);
       if (!isMatch)
-        return done(null, false, { message: "Incorrect Password." });
+        return done(null, false, { message: "Contraseña Incorrecta." });
       
       return done(null, user);
     }
