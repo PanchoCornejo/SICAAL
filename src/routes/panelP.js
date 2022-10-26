8edad759879043e1d5af3687dd8696a18209ffda
PanchoCornejo <jofracocapancho@gmail.com>
2022-10-26 02:50:13 -0300

    Proveedores 2

--- src/routes/panelP.js
+++ src/routes/panelP.js
@@ -16,15 +16,43 @@ router.get('/misservicios',isProveedor ,(req, res) => {
 });
 
 // Donde puede el Proveedor: mirar su perfil.
-router.get('/perfilP',isProveedor ,(req, res) => {
-    res.render('proveedores/perfilP');
+router.get('/perfilP',isProveedor, async (req, res) => {
+
+    console.log("Te dare los datos: " + req.user.id)
+    const id = req.user.id;
+    console.log("la id es: " +id)
+    const datos = await pool.query('SELECT * FROM proveedor WHERE user_id = ?', [id]);
+    console.log("Datos de la Consulta SQL");
+    console.log(datos);
+    res.render('proveedores/perfilP', {datos: datos[0]});
 });
+
+//router.get('/perfilP',isProveedor ,(req, res) => {
+//    res.render('proveedores/perfilP');
+//});
 
 
 // Donde puede el Proveedor: editar su perfil.
-router.get('/editar',isProveedor ,(req, res) => {
-    res.render('proveedores/editar');
+
+router.get('/editar',isProveedor,  async (req, res) => {
+    const id = req.user.id;
+    console.log(id)
+    const provee = await pool.query('SELECT * FROM proveedor WHERE id = ?', [id]);
+    console.log(provee[0]);
+    res.render('proveedores/editarP', {provee: provee[0]});
 });
 
+router.post('/editar', isProveedor ,  async (req, res) => {
+    const id = req.user.id;
+    const { title, description, url} = req.body; 
+    const newProvee = {
+        title,
+        description,
+        url
+    };
+    await pool.query('UPDATE proveedor set ? WHERE id = ?', [newProvee, id]);
+    req.flash('Logrado', 'Datos de proveedor Actualizados correctamente');
+    res.redirect('/panelP/perfilP');
+});
 
 module.exports = router;
